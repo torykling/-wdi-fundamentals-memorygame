d133c64d3ed0b7625966fdbf433a301b9b30e6a2
Tory <victoriaklingenstein@Victorias-MacBook-Pro.local>
2019-09-17 10:37:01 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,28 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay.length !== 2) {
+		return;
+	} else if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
-}
+};
+
+function flipCard(cardId) {
+	// if (cardsInPlay.length === 2) {
+	// if (cardOne === cardTwo) {
+	// 	alert("You found a match!");
+	// } else {
+	// 	alert("Sorry, try again.");
+	// }
+// }
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
