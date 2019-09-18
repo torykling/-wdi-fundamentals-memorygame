a992201a4a187603fecef6fd33931854d4cef02a
Tory <victoriaklingenstein@Victorias-MacBook-Pro.local>
2019-09-18 13:42:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,29 +23,32 @@ cardImage: "images/king-of-diamonds.png"
 const cardsInPlay = [];
 
 function checkForMatch() {
-	if (cardsInPlay.length !== 2) {
-		return;
-	} else if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 };
 
-function flipCard(cardId) {
-	// if (cardsInPlay.length === 2) {
-	// if (cardOne === cardTwo) {
-	// 	alert("You found a match!");
-	// } else {
-	// 	alert("Sorry, try again.");
-	// }
-// }
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+	}
+};
+createBoard();
